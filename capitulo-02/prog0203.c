47ff4dc2bf2d1ea1e22c5b231a5ea53d2c44c6b4
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-25 22:55:24 -0300

    prog0203

---
+++ capitulo-02/prog0203.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2;
+	printf("Introduza dois Números: ");
+	scanf("%d%d", &num1, &num2);
+	printf("O resuldado de %d + %d = %d\n", num1, num2, num1+num2);
+}
