818692c5b2e1482d85ca2e66f08335437c39ef61
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-25 23:10:22 -0300

    prog0205

---
+++ capitulo-02/prog0205.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("O Tamanho em bytes de um char = %ld\n", sizeof(char));
+	printf("O Tamanho em bytes de um int = %ld\n", sizeof(int));
+	printf("O Tamanho em bytes de um float = %ld\n", sizeof(float));
+	printf("O Tamanho em bytes de um double = %ld\n", sizeof(double));
+}
